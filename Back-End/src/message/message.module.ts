3201be556046be28c47561a7ebf3832efdbcf259
rohit prajapati <rohitprajapati@192.168.1.13>
2022-10-01 20:02:44 +0530

    added socket message gateway

--- Back-End/src/message/message.module.ts
+++ Back-End/src/message/message.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { PrismaModule } from 'src/prisma/prisma.module'
+import { UsersModule } from 'src/users/users.module'
 import { MessageController } from './message.controller'
 import { MessageService } from './message.service'
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule],
   controllers: [MessageController],
   providers: [MessageService],
 })
