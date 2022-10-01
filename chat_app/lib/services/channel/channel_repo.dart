8240aab5a14d214d2dbc3a0451f0a70ee2d26bbd
Tanmay Soni <tanmays@Tanmays-MacBook-Pro.local>
2022-10-01 22:50:27 +0530

    home page

--- chat_app/lib/services/channel/channel_repo.dart
+++ chat_app/lib/services/channel/channel_repo.dart
@@ -32,4 +32,27 @@ class ChannelRepository {
       rethrow;
     }
   }
+
+  Future<List<ChannelModal>> findTrendingChannel() async {
+    try {
+      final Response response = await dio.get(
+        'channels/trending',
+        options: Options(
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            'Content-Type': 'application/json',
+            'Accept': '*/*',
+            'Authorization':
+                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3ZTFmYjc3MS01MGU4LTRhMjUtODA0Ny1hYjMwODI0MzBkOWYiLCJlbWFpbCI6InRlc3RAZW1haWwuY29tIiwiaWF0IjoxNjY0NjMyNjkwLCJleHAiOjkwMDAxNjY0NjMyNjkwfQ.q72K88De93bfNgqhDgDdlO6t4_Uz8QZp_uN10kkjQs8'
+          },
+        ),
+      );
+
+      final data = List<ChannelModal>.from(
+          response.data['data'].map((e) => ChannelModal.fromJson(e)).toList());
+      return data;
+    } catch (_) {
+      rethrow;
+    }
+  }
 }
